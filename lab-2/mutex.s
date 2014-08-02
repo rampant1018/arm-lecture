83a72c2730c10f4016ba15f1510f022ad9e5aab6
Rampant <rampant1018@gmail.com>
2014-08-02 12:03:28 +0800

    Implement mutex

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -5,21 +5,35 @@
 	.equ locked, 1
 	.equ unlocked, 0
 
+@ void lock_mutex(void *mutex)
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+.L1:
+	ldr r1, =locked
+	ldrex r2, [r0]
+	cmp r2, r1
+	beq .L1
+	strexne r2, r1, [r0]
+	cmpne r2, #1
+	beq .L1
+	dmb
         @ END CODE INSERT
 	bx lr
 
 	.size lock_mutex, .-lock_mutex
 
+@ void unlock_mutex(void *mutex)
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+	ldr r1, =unlocked
+	dmb
+	str r1, [r0]
+	dsb
+	sev
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
