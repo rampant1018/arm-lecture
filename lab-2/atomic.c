fddec0ba2a476849c004ac0785f51fd4e477856c
Rampant <rampant1018@gmail.com>
2014-08-02 12:03:11 +0800

    Define thread number

--- lab-2/atomic.c
+++ lab-2/atomic.c
@@ -7,17 +7,17 @@
 #define locked 1
 #define unlocked 0
 
+#define THREAD_NUM 2
+
 extern void lock_mutex(void *mutex);
 extern void unlock_mutex(void *mutex);
 
-pthread_t tid[2];
+pthread_t tid[THREAD_NUM];
 int counter;
-// pthread_mutex_t lock;
 unsigned int mutexlock = unlocked;
 
 void *doSomeThing(void *arg)
 {
-	// pthread_mutex_lock(&lock);
 	lock_mutex(&mutexlock); 
 	unsigned long i = 0;
 	counter += 1;
@@ -27,7 +27,6 @@ void *doSomeThing(void *arg)
 
 	printf("\n Job %d finished\n", counter);
 
-	// pthread_mutex_unlock(&lock);
 	unlock_mutex(&mutexlock);
 	return NULL;
 }
@@ -37,23 +36,16 @@ int main(void)
 	int i = 0;
 	int err;
 
-#if 0
-	if (pthread_mutex_init(&lock, NULL) != 0) {
-		printf("\n mutex init failed\n");
-		return 1;
-	}
-#endif
-
-	while (i < 2) {
+	while (i < THREAD_NUM) {
 		err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
 		if (err != 0)
 			printf("\ncan't create thread :[%s]", strerror(err));
 		i++;
 	}
 
-	pthread_join(tid[0], NULL);
-	pthread_join(tid[1], NULL);
-	// pthread_mutex_destroy(&lock);
+	for(i = 0; i < THREAD_NUM; i++) {
+		pthread_join(tid[i], NULL);
+	}
 
 	return 0;
 }
